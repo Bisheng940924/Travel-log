3d10e319bd749f82476ef26bdcb31db044c8e20b
Bisheng <bisheng940924@gmail.com>
2020-03-02 02:47:25 +0800

    complete logs create and query api

--- server/src/index.js
+++ server/src/index.js
@@ -2,22 +2,34 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const logs = require('./api/logs')
+require('dotenv').config();
 
 
 const middleware = require('./middleware')
-
 const app = express();
+
+console.log(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
 }));
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({
         message: "Hello World"
     });
 });
+
+app.use('/api/logs', logs)
 
 app.use(middleware.notFound);
 app.use(middleware.errorHandler);
